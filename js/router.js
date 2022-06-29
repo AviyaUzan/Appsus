ec52df81557f96ba0ad147b8625c845a93820508
Aviya Uzan <aviyauzan@gmail.com>
2022-06-29 10:42:48 +0300

    more basic files

--- js/router.js
+++ js/router.js
@@ -1,10 +1,29 @@
-import homePage from './views/home-page.cmp.js'
+import homePage from './pages/home-page.cmp.js'
+import mailApp from "./pages/mail-app.cmp.js";
+import noteApp from "./pages/notes-app.cmp.js";
+import bookApp from "./pages/books-app.cmp.js";
 
 const routes = [
 	{
 		path: '/',
 		component: homePage
-	}
+	},
+	{
+        path: '/mail',
+        component: mailApp,
+	},
+	{
+        path: '/note',
+        component: noteApp
+    },
+	{
+        path: '/book',
+        component: bookApp
+    },
+    // {
+    //     path: '/book/:bookId',
+    //     component: bookDetails
+    // },
 ]
 
 export const router = VueRouter.createRouter({
